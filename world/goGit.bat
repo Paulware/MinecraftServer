1193dbbc2a3e41d2588fb75bf2af0d1ed1ef82f5
Paulware <Paulware@hotmail.com>
2020-05-24 11:38:21 -0500

    spleef

---
+++ world/goGit.bat
@@ -0,0 +1,9 @@
+copy C:\SpigotMC\scriptcraft\plugins\spleef.js
+copy C:\SpigotMC\scriptcraft\plugins\spleef.xml
+copy C:\SpigotMC\spigot.yml
+copy C:\SpigotMC\bukkit.yml
+cd ..
+git add .
+git commit -am "comment"
+git push origin master
+pause
