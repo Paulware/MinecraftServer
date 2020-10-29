38d12c2f3a58114c8f76f86a9c6afaf32ae5562b
Paul Richards <paulware@hotmail.com>
2020-10-28 19:05:34 -0500

    end of game

--- scriptcraft/plugins/test.js
+++ scriptcraft/plugins/test.js
@@ -145,6 +145,15 @@ function listeners () {
   events.projectileHit( function (event) {
     projectileHit(event);
   });
+  // Set gameId so we know that null is not associated with any player
+  events.playerJoin( function (event) {
+    fd = new org.bukkit.metadata.FixedMetadataValue (__plugin,0);
+    if ((event.getPlayer== null) ? null : event.getPlayer() != null) {
+      if ((event.getPlayer== null) ? null : event.getPlayer().setMetadata != null ) {
+        (event.getPlayer== null) ? null : event.getPlayer().setMetadata ("_gameid_", fd );
+      }
+    }
+  });
 };
 
 // If the player moves the code does something
@@ -161,49 +170,56 @@ function PlayerMoved (event) {
   player=(event.getPlayer== null) ? null : event.getPlayer();
   gameId=(player== null)? null : (player.getMetadata == null)?null:(player.getMetadata("_gameid_").length == 0)?null:player.getMetadata("_gameid_")[0].value();
   team=(player== null)? null : (player.getMetadata == null)?null:(player.getMetadata("_team_").length == 0)?null:player.getMetadata("_team_")[0].value();
-  if (((gameId) != (exports.gameId))){
-    (function() {
-      if (player != null ) {
-         player.sendMessage ("Welcome to the game v1.1");
-      }
-     })();
-    fd = new org.bukkit.metadata.FixedMetadataValue (__plugin,exports.gameId);
-    if (player != null) {
-      if (player.setMetadata != null ) {
-        player.setMetadata ("_gameid_", fd );
-      }
-    }
-    player.removeMetadata ("_team_", __plugin );
-    (function () { var _pitch; var _yaw; var _velocity;
-      _pitch    = player.location.getPitch();
-      _yaw      = player.location.getYaw();
-      _velocity = player.getVelocity();
-      exports.lobbyLocation.setPitch (_pitch);
-      exports.lobbyLocation.setYaw   (_yaw);
-      player.teleport(exports.lobbyLocation, org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN);
-      player.setVelocity(_velocity);
-    })();
-    player.setGameMode(org.bukkit.GameMode.SURVIVAL);
-  }
-  if (((team) == ("red"))){
-    defenders=exports.blueDefenders;
+  // Join server sets gameId to 0 so this should
+  // never occur. Code is here for debugging only.
+  if (((gameId) == (null))){
+    console.log ("This should not happen for a player: " + player.name);
   }
   else {
-    defenders=exports.redDefenders;
-  }
-  for (var _elementIndex=0; _elementIndex <defenders.length;_elementIndex++) {
-    loc = defenders[_elementIndex];
-    distance=loc.distance((player== null)?null:player.location);
-    if (((distance) < (20))){
-      newLocation=(function() { var _x = loc.x + 0;var _y = loc.y + 3;var _z = loc.z + 0;var _loc = new org.bukkit.Location(server.worlds[0],_x,_y,_z);return _loc; })();
-      vector=(player== null)?null:player.location.toVector().subtract(newLocation.toVector());
-      vector=vector.multiply (1.5);
-      for (var i=0; i<5; i++) {
-        arrow=server.worlds[0].spawnArrow(newLocation,vector,vector.length(),12);
-        ;
+    if (((gameId) != (exports.gameId))){
+      (function() {
+        if (player != null ) {
+           player.sendMessage ("Welcome to the game v1.1");
+        }
+       })();
+      fd = new org.bukkit.metadata.FixedMetadataValue (__plugin,exports.gameId);
+      if (player != null) {
+        if (player.setMetadata != null ) {
+          player.setMetadata ("_gameid_", fd );
+        }
       }
+      player.removeMetadata ("_team_", __plugin );
+      (function () { var _pitch; var _yaw; var _velocity;
+        _pitch    = player.location.getPitch();
+        _yaw      = player.location.getYaw();
+        _velocity = player.getVelocity();
+        exports.lobbyLocation.setPitch (_pitch);
+        exports.lobbyLocation.setYaw   (_yaw);
+        player.teleport(exports.lobbyLocation, org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN);
+        player.setVelocity(_velocity);
+      })();
+      player.setGameMode(org.bukkit.GameMode.SURVIVAL);
     }
-  };
+    if (((team) == ("red"))){
+      defenders=exports.blueDefenders;
+    }
+    else {
+      defenders=exports.redDefenders;
+    }
+    for (var _elementIndex=0; _elementIndex <defenders.length;_elementIndex++) {
+      loc = defenders[_elementIndex];
+      distance=loc.distance((player== null)?null:player.location);
+      if (((distance) < (20))){
+        newLocation=(function() { var _x = loc.x + 0;var _y = loc.y + 3;var _z = loc.z + 0;var _loc = new org.bukkit.Location(server.worlds[0],_x,_y,_z);return _loc; })();
+        vector=(player== null)?null:player.location.toVector().subtract(newLocation.toVector());
+        vector=vector.multiply (1.5);
+        for (var i=0; i<5; i++) {
+          arrow=server.worlds[0].spawnArrow(newLocation,vector,vector.length(),12);
+          ;
+        }
+      }
+    };
+  }
 };
 
 function PlayerDied (event) {
@@ -304,6 +320,7 @@ function PlayerClick (event) {
   var durability;
   var itemType;
   var newLocation;
+  var career;
   player=(event.getPlayer== null) ? null : event.getPlayer();
   block=(event.getClickedBlock== null) ? null : event.getClickedBlock();
   hand=(event.getHand== null) ? null : event.getHand();
@@ -324,7 +341,7 @@ function PlayerClick (event) {
         }
        })();
     }
-    else if (((durability) >= 0)){
+    else if (((durability) >= 400)){
       careerInfantry(player,hand,action);
     }
     else {
@@ -352,8 +369,13 @@ function PlayerClick (event) {
         }
       }
       else {
-        careerFighter (player,block,hand,action);
-        careerBomber (player,block,hand,action);
+        career=(player== null)? null : (player.getMetadata == null)?null:(player.getMetadata("_career_").length == 0)?null:player.getMetadata("_career_")[0].value();
+        if (((career) == ("fighter"))){
+          careerFighter (player,block,hand,action);
+        }
+        else if (((career) == ("bomber"))){
+          careerBomber (player,block,hand,action);
+        }
       }
     }
   }
@@ -409,14 +431,14 @@ function careerInfantry (player,hand,action) {
                          .build());
       firework.setFireworkMeta(_meta);
       vector=(player== null)?null:player.location.getDirection().normalize();
-      vector=vector.multiply (2);
+      vector=vector.multiply (3);
       firework.setVelocity(vector);
     }
   }
   else {
     (function() {
       if (player != null ) {
-         player.sendMessage ("Sorry you cannot fly, your elytra is damaged");
+         player.sendMessage ("Sorry you cannot fly, your elytra is damaged.  You are now infantry");
       }
      })();
   }
@@ -476,65 +498,57 @@ function selectCareer (block,player) {
   }
 };
 
+function careerBomber (player,block,hand,action) {
+  //Instantiations;
+  var newLocation;
+  if (((hand) == (org.bukkit.inventory.EquipmentSlot.HAND))){
+    if (((action) == (org.bukkit.event.block.Action.LEFT_CLICK_AIR))){
+      newLocation=(function() { var _x = (player== null)?null:player.location.x + 0;var _y = (player== null)?null:player.location.y + 10;var _z = (player== null)?null:player.location.z + 0;var _loc = new org.bukkit.Location(server.worlds[0],_x,_y,_z);return _loc; })();
+      (function () { var _pitch; var _yaw; var _velocity;
+        _pitch    = player.location.getPitch();
+        _yaw      = player.location.getYaw();
+        _velocity = player.getVelocity();
+        newLocation.setPitch (_pitch);
+        newLocation.setYaw   (_yaw);
+        player.teleport(newLocation, org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN);
+        player.setVelocity(_velocity);
+      })();
+    }
+  }
+};
+
 function careerFighter (player,block,hand,action) {
   //Instantiations;
-  var career;
   var vector;
   var newLocation;
   var arrow;
   var stack;
   var material;
-  career=(player== null)? null : (player.getMetadata == null)?null:(player.getMetadata("_career_").length == 0)?null:player.getMetadata("_career_")[0].value();
-  if (((career) == ("fighter"))){
-    if (((hand) == (org.bukkit.inventory.EquipmentSlot.OFF_HAND))){
-      console.log ("action: " + action );
-      if (((action) == (org.bukkit.event.block.Action.RIGHT_CLICK_AIR))){
-        vector=(player== null)?null:player.location.getDirection().normalize();
-        vector=vector.multiply (5);
-        newLocation=(function() { var _x = (player== null)?null:player.location.x + 0;var _y = (player== null)?null:player.location.y + 4;var _z = (player== null)?null:player.location.z + 0;var _loc = new org.bukkit.Location(server.worlds[0],_x,_y,_z);return _loc; })();
-        for (var i=0; i<10; i++) {
-          arrow=server.worlds[0].spawnArrow(newLocation,vector,vector.length(),12);
-          ;
-        }
-      }
-    }
-    else if (((hand) == (org.bukkit.inventory.EquipmentSlot.HAND))){
-      stack=(player== null) ? null : ( player.getInventory().getItemInOffHand == null) ? null : player.getInventory().getItemInOffHand();
-      material=(stack == null ) ? null : (stack.getType == null) ? null : stack.getType();
-      console.log ("Material: " + material);
-      if (((material) == ("ARROW"))){
-        if (((action) == (org.bukkit.event.block.Action.LEFT_CLICK_AIR))){
-          if (stack.getAmount() > 0) {
-            stack.setAmount(stack.getAmount ()-1);
-          }
-          vector=(player== null)?null:player.location.getDirection().normalize();
-          vector=vector.multiply (2);
-          player.setVelocity(vector);
-          console.log ("Players vector updated");
-        }
+  if (((hand) == (org.bukkit.inventory.EquipmentSlot.OFF_HAND))){
+    console.log ("action: " + action );
+    if (((action) == (org.bukkit.event.block.Action.RIGHT_CLICK_AIR))){
+      vector=(player== null)?null:player.location.getDirection().normalize();
+      vector=vector.multiply (5);
+      newLocation=(function() { var _x = (player== null)?null:player.location.x + 0;var _y = (player== null)?null:player.location.y + 4;var _z = (player== null)?null:player.location.z + 0;var _loc = new org.bukkit.Location(server.worlds[0],_x,_y,_z);return _loc; })();
+      for (var i=0; i<10; i++) {
+        arrow=server.worlds[0].spawnArrow(newLocation,vector,vector.length(),12);
+        ;
       }
     }
   }
-};
-
-function careerBomber (player,block,hand,action) {
-  //Instantiations;
-  var career;
-  var newLocation;
-  career=(player== null)? null : (player.getMetadata == null)?null:(player.getMetadata("_career_").length == 0)?null:player.getMetadata("_career_")[0].value();
-  if (((career) == ("bomber"))){
-    if (((hand) == (org.bukkit.inventory.EquipmentSlot.HAND))){
+  else if (((hand) == (org.bukkit.inventory.EquipmentSlot.HAND))){
+    stack=(player== null) ? null : ( player.getInventory().getItemInOffHand == null) ? null : player.getInventory().getItemInOffHand();
+    material=(stack == null ) ? null : (stack.getType == null) ? null : stack.getType();
+    console.log ("Material: " + material);
+    if (((material) == ("ARROW"))){
       if (((action) == (org.bukkit.event.block.Action.LEFT_CLICK_AIR))){
-        newLocation=(function() { var _x = (player== null)?null:player.location.x + 0;var _y = (player== null)?null:player.location.y + 10;var _z = (player== null)?null:player.location.z + 0;var _loc = new org.bukkit.Location(server.worlds[0],_x,_y,_z);return _loc; })();
-        (function () { var _pitch; var _yaw; var _velocity;
-          _pitch    = player.location.getPitch();
-          _yaw      = player.location.getYaw();
-          _velocity = player.getVelocity();
-          newLocation.setPitch (_pitch);
-          newLocation.setYaw   (_yaw);
-          player.teleport(newLocation, org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN);
-          player.setVelocity(_velocity);
-        })();
+        if (stack.getAmount() > 0) {
+          stack.setAmount(stack.getAmount ()-1);
+        }
+        vector=(player== null)?null:player.location.getDirection().normalize();
+        vector=vector.multiply (2);
+        player.setVelocity(vector);
+        console.log ("Players vector updated");
       }
     }
   }
